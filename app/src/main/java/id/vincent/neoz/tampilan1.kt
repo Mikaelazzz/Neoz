26d93e7004d62ddecd57116739f7c5901e4691e4
Mikaela <166426938+Mikaelazzz@users.noreply.github.com>
2024-10-24 00:11:11 +0700

    up

--- app/src/main/java/id/vincent/neoz/tampilan1.kt
+++ app/src/main/java/id/vincent/neoz/tampilan1.kt
@@ -2,6 +2,8 @@ package id.vincent.neoz
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,48 @@ class tampilan1 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_tampilan1)
 
+        val heroImage = intent.getIntExtra("image", 0)
+        val heroName = intent.getStringExtra("name")
+        val heroRole = intent.getStringExtra("role")
+        val heroBanRate = intent.getDoubleExtra("banRate", 0.0)
+        val heroType = intent.getStringExtra("type")
+        val bgHr = intent.getIntExtra("bghr", 0)
+        val imageR = intent.getIntExtra("imager",0)
+        val goldBp = intent.getStringExtra("gold")
+        val berlianDm = intent.getStringExtra("berlian")
+        val tiketMl = intent.getStringExtra("tikett")
+        val tierHr = intent.getIntExtra("tier",0)
+        val desTier = intent.getStringExtra("destier")
+        val laneHr = intent.getIntExtra("lane",0)
+        val tlaneHr = intent.getStringExtra("tlane")
+        val deslaneHr = intent.getStringExtra("deslane")
+
+
+        val bghero = findViewById<ImageView>(R.id.bghero)
+        val imgHero = findViewById<ImageView>(R.id.logohero)
+        val nameHero = findViewById<TextView>(R.id.titlehero)
+        val roleHero = findViewById<TextView>(R.id.rolehero)
+        val bpoin = findViewById <TextView>(R.id.bpoin)
+        val dm = findViewById <TextView>(R.id.dm)
+        val tiket = findViewById <TextView>(R.id.tiket)
+        val tierHero = findViewById<ImageView>(R.id.tierhero)
+        val desktier = findViewById<TextView>(R.id.desktier)
+        val heroLane = findViewById<ImageView>(R.id.heroLane)
+        val titleHeroLane = findViewById<TextView>(R.id.titleherolane)
+        val deskHeroLane = findViewById<TextView>(R.id.deskherolane)
+
+        bghero.setImageResource(bgHr)
+        imgHero.setImageResource(imageR)
+        nameHero.text = heroName ?: "Null"
+        roleHero.text = heroRole ?: "Null"
+        bpoin.text = goldBp ?: "0"
+        dm.text = berlianDm ?: "0"
+        tiket.text = tiketMl ?: "0"
+        tierHero.setImageResource(tierHr)
+        desktier.text = desTier ?: "Null"
+        heroLane.setImageResource(laneHr)
+        titleHeroLane.text = tlaneHr ?: "Null"
+        deskHeroLane.text = deslaneHr ?: "Null"
 
         // Mengaktifkan tombol back pada action bar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
