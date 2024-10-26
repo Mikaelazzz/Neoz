2aed6b12fe2de62e19740d7b345ba3187bc9de83
Mikaela <166426938+Mikaelazzz@users.noreply.github.com>
2024-10-27 00:08:45 +0700

    Update kalkulator.kt

--- app/src/main/java/id/vincent/neoz/kalkulator.kt
+++ app/src/main/java/id/vincent/neoz/kalkulator.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class KalkulatorFragment : Fragment() {
+class kalkulator : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.kalkulator, container, false)
@@ -19,9 +19,8 @@ class KalkulatorFragment : Fragment() {
 override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    // Inisialisasi tombol dan tambahkan onClickListener
     val btnWin: Button = view.findViewById(R.id.win)
-    val btnLose: Button = view.findViewById(R.id.lose)
+    val btnlose: Button = view.findViewById(R.id.lose)
     val btnMagicWheel: Button = view.findViewById(R.id.mgcw)
     val btnZodiac: Button = view.findViewById(R.id.zdc)
     val btnTotalMatch: Button = view.findViewById(R.id.totalmatchh)
@@ -31,8 +30,8 @@ override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         startActivity(intent)
     }
 
-    btnLose.setOnClickListener {
-        val intent = Intent (requireContext(),win::class.java)
+    btnlose.setOnClickListener {
+        val intent = Intent (requireContext(),lose::class.java)
         startActivity(intent)
     }
 
