40fe6c637f7ee484812531bbc59ad5c687eb2513
Mikaela <166426938+Mikaelazzz@users.noreply.github.com>
2024-11-06 20:16:56 +0700

    Update lainnya.kt

--- app/src/main/java/id/vincent/neoz/lainnya.kt
+++ app/src/main/java/id/vincent/neoz/lainnya.kt
@@ -38,7 +38,9 @@ class lainnya : AppCompatActivity() {
         val tlane: String,
         val deslane: String,
         val desbefore: String,
-        val desafter: String
+        val desafter: String,
+        val patchimg : Int,
+        val textpatch : String
     )
 
 
@@ -63,11 +65,11 @@ class lainnya : AppCompatActivity() {
         val mid = getString(R.string.mid)
 
         val heroes = listOf(
-            Hero(R.drawable.gambar1, "Ling", "Assasin", 26.27, "Chase / Burst", R.drawable.serene, R.drawable.hero1,"32000","599", "0", R.drawable.jung, Splus,R.drawable.jung,"Jungle",jungle,"null", "null" ),
-            Hero(R.drawable.gambar1, "Freya", "Fighter", 1.26, "Chase / Damage", R.drawable.freya, R.drawable.hero2,"0","599", "0", R.drawable.gold,Splus,R.drawable.gold,"Gold",gold,"null", "null" ),
-            Hero(R.drawable.gambar1, "Aldous", "Fighter", 20.5, "Damage", R.drawable.serene, R.drawable.hero3,"32000","599", "0", R.drawable.exp, B,R.drawable.exp,"Exp",exp,"null", "null" ),
-            Hero(R.drawable.gambar1, "Selena", "Assassin / Mage", 18.75, "Damage / Burst", R.drawable.freya, R.drawable.hero4,"32000","599", "0", R.drawable.roam,B,R.drawable.roam,"Roam",roam,"null", "null" ),
-            Hero(R.drawable.gambar1, "Lancelot", "Assassin", 18.75, "Damage / Burst", R.drawable.serene, R.drawable.hero5,"32000","599", "0", R.drawable.jung,A,R.drawable.jung,"Jungle", jungle,"null", "null" )
+            Hero(R.drawable.gambar1, "Ling", "Assasin", 26.27, "Chase / Burst", R.drawable.serene, R.drawable.hero1,"32000","599", "0", R.drawable.jung, Splus,R.drawable.jung,"Jungle",jungle,"null", "null", R.drawable.buff, "Buff" ),
+            Hero(R.drawable.gambar1, "Freya", "Fighter", 1.26, "Chase / Damage", R.drawable.freya, R.drawable.hero2,"0","599", "0", R.drawable.gold,Splus,R.drawable.gold,"Gold",gold,"null", "null", R.drawable.buff, "Buff" ),
+            Hero(R.drawable.gambar1, "Aldous", "Fighter", 20.5, "Damage", R.drawable.serene, R.drawable.hero3,"32000","599", "0", R.drawable.exp, B,R.drawable.exp,"Exp",exp,"null", "null", R.drawable.buff, "Buff" ),
+            Hero(R.drawable.gambar1, "Selena", "Assassin / Mage", 18.75, "Damage / Burst", R.drawable.freya, R.drawable.hero4,"32000","599", "0", R.drawable.roam,B,R.drawable.roam,"Roam",roam,"null", "null", R.drawable.buff, "Buff" ),
+            Hero(R.drawable.gambar1, "Lancelot", "Assassin", 18.75, "Damage / Burst", R.drawable.serene, R.drawable.hero5,"32000","599", "0", R.drawable.jung,A,R.drawable.jung,"Jungle", jungle,"null", "null", R.drawable.buff, "Buff" )
         )
 
         val recyclerView = findViewById<RecyclerView>(R.id.heroRecyclerView)
